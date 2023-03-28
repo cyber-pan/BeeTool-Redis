125eb949bfb2bd93bfa34abc454f09422237aa85
cyber.pan <PAN02375640597>
2023-03-28 13:43:44 +0800

    调整helper

---
+++ beetool-redis-robin/src/test/java/org/beetool/redis/FileTest.java
@@ -0,0 +1,31 @@
+package org.beetool.redis;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author cyber.pan
+ * @Classname FileTest
+ * @Description TODO
+ * @Date 2023/3/28 12:17
+ */
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = App.class)
+public class FileTest {
+
+    @Test
+    public void fileTest() {
+        ClassPathResource classPathResource = new ClassPathResource("redis/ZetRobinQueue_addScoreWithLimitScore.lua");
+        ClassPathResource classPathResource2 = new ClassPathResource("application.properties");
+
+        Boolean bool = classPathResource.exists();
+        Boolean bool2 = classPathResource2.exists();
+        System.out.println();
+    }
+
+}
