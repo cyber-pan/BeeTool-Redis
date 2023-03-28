460f639e92884777b793d41e48d8f7c825aa9b95
cyber.pan <PAN02375640597>
2023-03-28 20:31:42 +0800

    修改

--- beetool-redis-robin/src/main/java/org/beetool/redis/robin/loop/queue/model/connector/serializer/FastJson2JsonRedisParamSerializer.java
+++ beetool-redis-robin/src/main/java/org/beetool/redis/robin/loop/queue/model/connector/serializer/FastJson2JsonRedisParamSerializer.java
@@ -1,17 +1,17 @@
 package org.beetool.redis.robin.loop.queue.model.connector.serializer;
 
+import cn.hutool.json.JSONUtil;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.parser.ParserConfig;
 import com.alibaba.fastjson.serializer.SerializerFeature;
-
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.data.redis.serializer.SerializationException;
 import org.springframework.util.Assert;
 
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Optional;
 
@@ -32,7 +32,7 @@ public class FastJson2JsonRedisParamSerializer<T> implements RedisSerializer<T> 
 
     private ObjectMapper objectMapper = new ObjectMapper();
 
-    public static final Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
+    public static final Charset DEFAULT_CHARSET = Charset.forName(StandardCharsets.UTF_8.name());
     /**
      * 基础类型参数
      */
@@ -60,7 +60,7 @@ public class FastJson2JsonRedisParamSerializer<T> implements RedisSerializer<T> 
     }
 
     @Override
-    public byte[] serialize(T t) throws SerializationException {
+    public byte[] serialize(T t) {
         if (t == null) {
             return new byte[0];
         }
@@ -75,17 +75,13 @@ public class FastJson2JsonRedisParamSerializer<T> implements RedisSerializer<T> 
     }
 
     @Override
-    public T deserialize(byte[] bytes) throws SerializationException {
+    public T deserialize(byte[] bytes) {
         if (bytes == null || bytes.length <= 0) {
             return null;
         }
         String str = new String(bytes, DEFAULT_CHARSET);
-        return Optional.ofNullable(clazz).map(c -> {
-            if (str.startsWith("{") || str.startsWith("[")) {
-                return JSON.parseObject(str, c);
-            }
-            return (T) str;
-        }).orElse((T) str);
+        return Optional.ofNullable(clazz).map(c -> JSONUtil.isJson(str) ? JSON.parseObject(str, c) : (T) str)
+                .orElse((T) str);
     }
 
     public void setObjectMapper(ObjectMapper objectMapper) {
